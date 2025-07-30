8f3f5f7a4f8268a7a6179018a2de4d5ca4877f69
VaibhavTalkhande <vaibhavtalkhande41@gmail.com>
2025-07-30 23:22:13 +0530

    basic pagination

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-
+import { Paginator, type PaginatorPageChangeEvent } from 'primereact/paginator';
+        
 // title, place_of_origin, artist_display, inscriptions, date_start, date_end
 
 interface Product {
@@ -16,10 +17,10 @@ interface Product {
 
 export default function BasicDemo() {
     const [products, setProducts] = useState<Product[]>([]);
-
+    const [page, setPage] = useState<number>(1);
     useEffect(() => {
         const fetchProducts = async () => {
-            const response = await fetch('https://api.artic.edu/api/v1/artworks?page=1');
+            const response = await fetch(`https://api.artic.edu/api/v1/artworks?page=${page}`);
             const data = await response.json();
             setProducts(data.data.map((item: Product) => ({
                 id: item.id,
@@ -33,20 +34,24 @@ export default function BasicDemo() {
         };
 
         fetchProducts();
-    }, []);
+    }, [page]);
 
     return (
-        <div className="p-4">
-            <DataTable value={products} tableStyle={{ minWidth: '50rem' }} className='border-2 border-blue-500 p-2 border-solid'>
-                <Column field="id" header="ID" className='border-1 border-blue-500'></Column>
-                <Column field="title" header="Title" className='border-1 border-blue-500'></Column>
-                <Column field="place_of_origin" header="Place of Origin" className='border-1 border-blue-500'></Column>
-                <Column field="artist_display" header="Artist Display" className='border-1 border-blue-500'></Column>
-                <Column field="inscriptions" header="Inscriptions" className='border-1 border-blue-500'></Column>
-                <Column field="date_start" header="Date Start" className='border-1 border-blue-500'></Column>
-                <Column field="date_end" header="Date End" className='border-1 border-blue-500'></Column>
-            </DataTable>
-        </div>
+        <>
+            <h2 className="text-2xl font-bold mb-4">Artworks</h2>
+            <div className="p-4">
+                <DataTable value={products} tableStyle={{ minWidth: '50rem' }} className='border-2 border-blue-500 p-2 border-solid'>
+                    <Column field="id" header="ID" className='border-1 border-blue-500'></Column>
+                    <Column field="title" header="Title" className='border-1 border-blue-500'></Column>
+                    <Column field="place_of_origin" header="Place of Origin" className='border-1 border-blue-500'></Column>
+                    <Column field="artist_display" header="Artist Display" className='border-1 border-blue-500'></Column>
+                    <Column field="inscriptions" header="Inscriptions" className='border-1 border-blue-500'></Column>
+                    <Column field="date_start" header="Date Start" className='border-1 border-blue-500'></Column>
+                    <Column field="date_end" header="Date End" className='border-1 border-blue-500'></Column>
+                </DataTable>
+            </div>
+                 <Paginator first={page} rows={10} totalRecords={120} rowsPerPageOptions={[10, 20, 30]} onPageChange={(current)=>{setPage(current.page)}} />
+        </>
     );
 }
         
